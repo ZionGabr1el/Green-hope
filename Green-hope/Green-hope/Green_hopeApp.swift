231195c53956b50d402d135fda11c560898bf22f
Zion <89410103+ZionGabr1el@users.noreply.github.com>
2022-06-10 11:02:52 -0300

    Add files via upload

---
+++ Green-hope/Green-hope/Green_hopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Green_hopeApp.swift
+//  Green-hope
+//
+//  Created by aluno on 10/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Green_hopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
